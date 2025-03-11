20b677390c74b90fb1b1872c3642630c8e117df8
fox-yuto-higo <yuto.higo@fox-hound.jp>
2025-03-11 18:18:36 +0900

    DeleteUserBottunの実装

---
+++ components/DeleteUserButton.tsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import { hideUser } from "../utils/api"; // hideUser関数のインポート
+import { Button } from "@mui/material";
+
+
+interface DeleteUserButtonProps {
+    userId: number;
+    onDelete: (userId: number) => void; //再レンダリング用
+}
+
+const DeleteUserButton: React.FC<DeleteUserButtonProps> = ({ userId, onDelete }) => {
+    const [error, setError] = useState<string | null>(null);
+
+    const handleHideUser = async () => {
+        setError(null);
+        if (confirm("本当にこのユーザーを削除しますか？")) {
+            try {
+                await hideUser(userId);
+                onDelete(userId);
+            } catch (err) {
+                setError("ユーザーの削除に失敗しました");
+            }
+        }
+    };
+
+    return (
+        <div>
+            <Button onClick={handleHideUser} >
+                ユーザーを削除
+            </Button>
+            {error && <p style={{ color: "red" }}>{error}</p>}
+        </div>
+    )
+
+}
+
+export default DeleteUserButton;

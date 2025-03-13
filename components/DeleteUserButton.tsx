c683a73f839fdc287583342c2d6ba7bd05a7480f
fox-yuto-higo <yuto.higo@fox-hound.jp>
2025-03-13 14:15:32 +0900

    DeletedButtonの改修

--- components/DeleteUserButton.tsx
+++ components/DeleteUserButton.tsx
@@ -1,32 +1,39 @@
 import { useState } from "react";
-import { hideUser } from "../utils/api"; // hideUser関数のインポート
+import { hideUser } from "../utils/api";
 import CustomButton from "@/components/parts/CustomButton";
+import CustomModal from "./parts/CustomModal";
 
 interface DeleteUserButtonProps {
     userId: number;
-    onDelete: (userId: number) => void; //再レンダリング用
+    onDelete: (userId: number) => void;
 }
 
 const DeleteUserButton: React.FC<DeleteUserButtonProps> = ({ userId, onDelete }) => {
     const [error, setError] = useState<string | null>(null);
+    const [modalOpen, setModalOpen] = useState(false);
 
     const handleHideUser = async () => {
         setError(null);
-        if (confirm("本当にこのユーザーを削除しますか？")) {
             try {
                 await hideUser(userId);
                 onDelete(userId);
             } catch (err) {
                 setError("ユーザーの削除に失敗しました");
             }
-        }
     };
 
     return (
         <div>
-            <CustomButton onClick={handleHideUser} size="small" variantType="danger">
+            <CustomButton onClick={() => setModalOpen(true)} size="small" variantType="danger">
                 ユーザーを削除
             </CustomButton>
+            <CustomModal
+                open={modalOpen}
+                title="確認"
+                content="本当にこのユーザーを削除しますか？"
+                onClose={() => setModalOpen(false)}
+                onConfirm={handleHideUser}
+            />
             {error && <p style={{ color: "red" }}>{error}</p>}
         </div>
     )
